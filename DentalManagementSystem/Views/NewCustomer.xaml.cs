70a2fef981996b503418c3a302bcbddae63304e9
JOSE RETAMAL - STUDENT <G00351330@gmit.ie>
2019-02-08 14:22:04 +0000

    Clear and error check on addd new customer

--- DentalManagementSystem/Views/NewCustomer.xaml.cs
+++ DentalManagementSystem/Views/NewCustomer.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,14 +28,44 @@ namespace DentalManagementSystem.Views
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Clear all inputs in view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ClearButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
+            inputName.Text = "";
+            inputSurename.Text = "";
+            idInput.Text = "";
+            emaillInput.Text = "";
+            streetInput.Text = "";
+            cityInput.Text = "";
+            provinceInput.Text = "";
+            countryInput.Text = "";
+            postCodeInput.Text = "";
+            mobilNumInput.Text = "";
+            homeNumInput.Text = "";
+            birthDatePicker.Date = new DateTime();
 
         }
 
+
         private void AddButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
-
+            if (inputName.Text == "")
+            {
+                topTextBlock.Text = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("Strings").GetString("NameError/Text");
+                topTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+            }else if(inputSurename.Text == "")
+            {
+                topTextBlock.Text = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("Strings").GetString("SurnameError/Text");
+                topTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+            }else if (idInput.Text == "")
+            {
+                topTextBlock.Text = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("Strings").GetString("IDError/Text");
+                topTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+            }
         }
     }
 }
