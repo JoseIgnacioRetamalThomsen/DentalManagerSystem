8a0670eba4e3762b77bc9779cd6326a94c0b51cf
JOSE RETAMAL - STUDENT <G00351330@gmit.ie>
2019-02-09 12:52:29 +0000

    view all customers started

--- DentalManagementSystem/App.xaml.cs
+++ DentalManagementSystem/App.xaml.cs
@@ -76,7 +76,7 @@ namespace DentalManagementSystem
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(NewTreatment), e.Arguments);
+                    rootFrame.Navigate(typeof(ViewAllCustomers), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
