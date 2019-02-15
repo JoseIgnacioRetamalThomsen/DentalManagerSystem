50a8c67d2f863d3da1a728c30e87419bc04f48f9
JOSE RETAMAL - STUDENT <G00351330@gmit.ie>
2019-02-15 15:00:11 +0000

    Data grid

--- DentalManagerSys/Views/ViewAllCustomers.xaml.cs
+++ DentalManagerSys/Views/ViewAllCustomers.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,14 +26,18 @@ namespace DentalManagerSys.Views
     public sealed partial class ViewAllCustomers : Page
     {
         Customer customer;
+        Customer customer1;
         ObservableCollection<Customer> customerList = null;
         public ViewAllCustomers()
         {
             this.InitializeComponent();
             customerList = new ObservableCollection<Customer>();
-            customer = new Customer("G000352030", "mark", "ndipenoch", "2899-23-12", "1 Dublin Road", "Galway", "Connaught", "Ireland", "HP1009", "008795623", "00000", new DateTime(),"Is Test Marshall");
+            customer = new Customer("G000352030", "markxcvxczvxczvzxcvxcz", "ndipenochvbcxzvbxcvxcxcvz", "2899-23-12", "1 Dublin Road", "Galway", "Connaught", "Ireland", "HP1009", "008795623", "00000", new DateTime(),"Is Test Marshall");
+            customer1 = new Customer("G0003520302", "markxcvxczvxczvzxcvxcz", "ndipenochvbcxzvbxcvxcxcvz", "2899-23-12", "1 Dublin Road", "Galway", "Connaught", "Ireland", "HP1009", "008795623", "00000", new DateTime(),"Is Test Marshall");
+
+
             customerList.Add(customer);
-            customerList.Add(customer);
+            customerList.Add(customer1);
             CustomersListView.ItemsSource = customerList;
 
         }
@@ -51,6 +56,12 @@ namespace DentalManagerSys.Views
         private void AppBarButton_Click_1(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ViewCustomerButton.IsEnabled = true;
+            Debug.WriteLine("change");
         }
     }
 }
