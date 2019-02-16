76e340c506045429aec054dbfb1eb1631effdf11
NCHONGMAJE NDIPENOCH - STUDENT <G00352031@gmit.ie>
2019-02-16 13:34:46 +0000

    Update customer method added

--- DentalManagerSys/MainPage.xaml.cs
+++ DentalManagerSys/MainPage.xaml.cs
@@ -32,6 +32,8 @@ namespace DentalManagerSys
 
             DAO.InitializeDatabase();
             DAO.UpdateTreatment(new Treatment("1","text",7));
+            //DAO.UpdateCustomer("Y","Peter","oBREINS", "16/02/2019 00:00:00", "y", "y", "y", "y", "y", "y", "y", "y", "y");
+           
         }
         private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
